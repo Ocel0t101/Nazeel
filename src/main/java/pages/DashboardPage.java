8667c07e3f01250b22b8b5e09087b22f075c71ef
Abdulrahman Ghoniem <abdulrahman_ghoneim@outlook.com>
2025-03-27 08:59:16 +0200

    Project launch

---
+++ src/main/java/pages/DashboardPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class DashboardPage {
+    WebDriver driver;
+    public DashboardPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Locators
+    private final By userVerificationLaterButton = By.xpath("//*[contains(text(),'Later')]");
+    private final By reservationsMenuNode = By.cssSelector("[href = '/reservations']");
+
+    //Actions
+    public void clickOnUserVerificationLaterButton(){
+        driver.findElement(userVerificationLaterButton).click();
+    }
+    /*public ReservationsPage clickOnReservationsPage(){
+        driver.findElement(reservationsMenuNode).click();
+        return new ReservationsPage(driver);
+    }*/
+}
