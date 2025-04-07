7fbb84bf34d47bc6b6e5f501111d7d9f60b9c0c9
mai_ma <maymahdy1248@gamil.com>
2025-04-07 13:18:39 +0200

    new page created

---
+++ src/test/java/nazeel/pages/SuppliesUnitUsagesPage.java
@@ -0,0 +1,10 @@
+package nazeel.pages;
+
+import org.openqa.selenium.By;
+
+public class SuppliesUnitUsagesPage {
+
+    private final By categoryDropboxBy = By.cssSelector("input[placeholder='Category']");
+    private final By categoryOptionsBy = By.cssSelector("li[role='option']");
+
+}
