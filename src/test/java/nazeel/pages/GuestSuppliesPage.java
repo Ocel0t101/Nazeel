d89bc7c5e9fccd69bcc3bf1ce5c42e97e7312a46
Mahmoud Gamal <mahmoudgamal1999@gmail.com>
2025-04-07 13:57:24 +0200

    [ADD] select Category Dropbox and options in GuestSuppliesPage [ADD] GuestSuppliesTS which contains testcases of Guest Supplies module

--- src/test/java/nazeel/pages/GuestSuppliesPage.java
+++ src/test/java/nazeel/pages/GuestSuppliesPage.java
@@ -8,7 +8,12 @@ import static nazeel.TestBase.getRootDriver;
 public class GuestSuppliesPage {
     private final By categoryDropboxBy = By.cssSelector("input[placeholder='Category']");
     private final By categoryOptionsBy = By.cssSelector("li[role='option']");
-    private final static String URL = "https://staging.nazeel.net:9002/guest-supplies/create-supplies-order";
+    private final By supplyDropboxBy = By.cssSelector("input[placeholder='Supply']");
+    private final By quantityBy = By.cssSelector("input[placeholder='Supply']");
+    private final By UnitTypesBy = By.cssSelector("kendo-multiselect[name='unitType']");
+    private final By UnitNumberBy = By.cssSelector("kendo-multiselect[name='unitNumber']");
+    private final By appendButton = By.className("n-button n-button--primary");
+    public final static String URL = "https://staging.nazeel.net:9002/guest-supplies/create-supplies-order";
 
     private WebElement getCategoryDropbox(){
         return getRootDriver().findElement(categoryDropboxBy);
@@ -18,8 +23,12 @@ public class GuestSuppliesPage {
         return getRootDriver().findElements(categoryOptionsBy).get(index);
     }
 
-    public void clickButton(){
+    public void selectCategoryDropbox(){
+        getCategoryDropbox().click();
+    }
 
+    public void selectCategoryOption(int index){
+        getCategoryOption(index).click();
     }
 
 
