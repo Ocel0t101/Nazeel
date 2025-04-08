44e580f0a230ea2ccd74a4dd00ecb7e9d56b281d
Mahmoud Gamal <mahmoudgamal1999@gmail.com>
2025-04-08 16:22:06 +0200

    [TEST] add full coverage for Guest Supplies append action [ADD] SuppliesOrderPage class with URL of it's page [MOV] move GuestSuppliesPage and locate SuppliesOrderPage into new package called guest_supplies

---
+++ src/test/java/nazeel/pages/guest_supplies/GuestSuppliesPage.java
@@ -0,0 +1,195 @@
+package nazeel.pages.guest_supplies;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static nazeel.TestBase.getRootDriver;
+
+public class GuestSuppliesPage {
+    private final By categoryDropboxBy = By.cssSelector("kendo-combobox[name='category']>span>span");
+    private final By optionsBy = By.cssSelector("li[role='option']");
+    private final By supplyDropboxBy = By.cssSelector("kendo-combobox[name='supply']>span>span");
+    private final By quantityInputBy = By.cssSelector("input[placeholder='Quantity']");
+    private final By unitTypesInputBy = By.cssSelector("kendo-multiselect[name='unitType']");
+    private final By unitNumberInputBy = By.cssSelector("kendo-multiselect[name='unitNumber']");
+    private final By appendButtonBy = By.className("n-button--primary");
+    public final static String URL = "https://staging.nazeel.net:9002/guest-supplies/create-supplies-order";
+    private final By addedSuppliesTableBy = By.cssSelector("tbody>tr[role='row']");
+    private final By createAccomplishedOrderButtonBy = By.cssSelector(".popup__btn.popup__btn--blue.h-35.d-flex.flex-row-reverse");
+    private final By successToastBy = By.className("guest-supplies/supplies-order/edit/");
+    private final By quantityPerUnitValuesRowsBy = By.cssSelector("tbody[role='presentation']>tr>td:nth-child(3)");
+    private final By commentInputBy = By.cssSelector("div>textarea.dropdown-toggle");
+    private final By clearButtonsBy = By.cssSelector("span[role='button']");
+
+    /**
+     * Locators
+     */
+    private WebElement getCategoryDropbox() {
+        return getRootDriver().findElement(categoryDropboxBy);
+    }
+
+    private List<WebElement> getCategoryOptions() {
+        return getRootDriver().findElements(optionsBy);
+    }
+
+    private WebElement getSupplyDropbox() {
+        return getRootDriver().findElement(supplyDropboxBy);
+    }
+
+    private List<WebElement> getSupplyOptions() {
+        return getRootDriver().findElements(optionsBy);
+    }
+
+    private WebElement getQuantityTextBox() {
+        return getRootDriver().findElement(quantityInputBy);
+    }
+
+    private WebElement getUnitTypesMultiSelector() {
+        return getRootDriver().findElement(unitTypesInputBy);
+    }
+
+    private List<WebElement> getUnitTypesOptions() {
+        return getRootDriver().findElements(optionsBy);
+    }
+
+    private WebElement getUnitNumberMultiSelector() {
+        return getRootDriver().findElement(unitNumberInputBy);
+    }
+
+    private WebElement getUnitNumberOption(int index) {
+        return getRootDriver().findElements(optionsBy).get(index);
+    }
+
+    private WebElement getAppendButtonBy() {
+        return getRootDriver().findElement(appendButtonBy);
+    }
+
+    private List<WebElement> getAddedSuppliesTable() {
+        return getRootDriver().findElements(addedSuppliesTableBy);
+    }
+
+    private WebElement getCreateAccomplishedButton() {
+        return getRootDriver().findElement(createAccomplishedOrderButtonBy);
+    }
+
+    private WebElement getSuccessToast() {
+        return getRootDriver().findElement(successToastBy);
+    }
+
+    private List<WebElement> getQuantityPerUnitValuesRows() {
+        return getRootDriver().findElements(quantityPerUnitValuesRowsBy);
+    }
+
+    private WebElement getCommentTextBox() {
+        return getRootDriver().findElement(commentInputBy);
+    }
+
+    private List<WebElement> getClearButtons(){
+        return getRootDriver().findElements(clearButtonsBy);
+    }
+
+    /**
+     * Actions
+     */
+    public GuestSuppliesPage navigateToGuestSuppliesPage() {
+        getRootDriver().get(URL);
+        return this;
+    }
+
+    public GuestSuppliesPage clickCategoryDropbox() {
+        getCategoryDropbox().click();
+        return this;
+    }
+
+    public boolean isCategoryDropboxShown() {
+        return getCategoryDropbox().isDisplayed();
+    }
+
+    public GuestSuppliesPage selectCategoryOption(int index) {
+        getCategoryOptions().get(index).click();
+        return this;
+    }
+
+    public boolean isCategoryOptionsEmpty() {
+        return getCategoryOptions().isEmpty();
+    }
+
+    public GuestSuppliesPage clickSupplyDropbox() {
+        getSupplyDropbox().click();
+        return this;
+    }
+
+    public GuestSuppliesPage selectSupplyOption(int index) {
+        getSupplyOptions().get(index).click();
+        return this;
+    }
+
+    public boolean isSupplyOptionsEnabled() {
+        return getSupplyOptions().getFirst().isEnabled();
+    }
+
+    public GuestSuppliesPage insertQuantity(String quantity) {
+        getQuantityTextBox().sendKeys(String.valueOf(quantity));
+        return this;
+    }
+
+    public GuestSuppliesPage clickUnitTypes() {
+        getUnitTypesMultiSelector().click();
+        return this;
+    }
+
+    public GuestSuppliesPage selectUnitTypesOption(int index) {
+        getUnitTypesOptions().get(index).click();
+        return this;
+    }
+
+    public boolean isUnitTypesEmpty() {
+        return getUnitTypesOptions().isEmpty();
+    }
+
+    public GuestSuppliesPage clickUnitNumber() {
+        getUnitNumberMultiSelector().click();
+        return this;
+    }
+
+    public GuestSuppliesPage selectUnitNumberOption(int index) {
+        getUnitNumberOption(index).click();
+        return this;
+    }
+
+    public void clickAppendButton() {
+        getAppendButtonBy().click();
+    }
+
+    public int countOfAddedSupplies() {
+        return getAddedSuppliesTable().size();
+    }
+
+    public GuestSuppliesPage clickCreateAccomplishedButton() {
+        getCreateAccomplishedButton().click();
+        return this;
+    }
+
+    public boolean isSuccessToastDisplayed() {
+        return getSuccessToast().isDisplayed();
+    }
+
+    public String quantityPerUnitValueByIndex(int index) {
+        return getQuantityPerUnitValuesRows().get(index).getText();
+    }
+
+    public GuestSuppliesPage insertComment(String comment) {
+        getCommentTextBox().sendKeys(comment);
+        return this;
+    }
+
+    public int getClearButtonsCount(){
+        return getClearButtons().size();
+    }
+
+    public boolean isQuantityInputClear(){
+        return getQuantityTextBox().getText().isEmpty();
+    }
+}
--- src/test/java/nazeel/test_suites/GuestSuppliesTS.java
+++ src/test/java/nazeel/test_suites/GuestSuppliesTS.java
@@ -2,10 +2,13 @@ package nazeel.test_suites;
 
 import nazeel.TestBase;
 import nazeel.pages.DashboardPage;
-import nazeel.pages.GuestSuppliesPage;
+import nazeel.pages.guest_supplies.GuestSuppliesPage;
 import nazeel.pages.LoginPage;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static nazeel.TestBase.Waits.*;
@@ -30,7 +33,7 @@ public class GuestSuppliesTS extends TestBase {
         loginPage.selectPropertyByIndex(2);
     }
 
-    @Test(testName = "Request valid accomplished order", suiteName = "Guest Supplies")
+    @Test(testName = "TC01 - Request valid accomplished order", suiteName = "Guest Supplies")
     public void tc01ValidAccomplishedOrder() {
         explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(ExpectedConditions.urlToBe(DashboardPage.URL));
         suppliesPage.navigateToGuestSuppliesPage();
@@ -42,7 +45,7 @@ public class GuestSuppliesTS extends TestBase {
         explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> suppliesPage.isSupplyOptionsEnabled());
         implicitWait(WAIT_TEMP.getSeconds());
         suppliesPage.selectSupplyOption(0);
-        suppliesPage.insertQuantity(2)
+        suppliesPage.insertQuantity("2")
                 .clickUnitTypes();
         explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isUnitTypesEmpty());
         implicitWait(WAIT_TEMP.getSeconds());
@@ -51,7 +54,327 @@ public class GuestSuppliesTS extends TestBase {
         suppliesPage.clickUnitNumber()
                 .selectUnitNumberOption(1)
                 .clickAppendButton();
+        Assert.assertTrue(suppliesPage.countOfAddedSupplies() > 0, "Supply item not added!");
+        implicitWait(10);
+        suppliesPage.clickCreateAccomplishedButton();
+        try {
+            explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(drive -> suppliesPage.isSuccessToastDisplayed());
+        } catch (TimeoutException e) {
+            System.out.println("Success toast didn't display!");
+        }
+
+        try {
+            explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(ExpectedConditions.urlContains(GuestSuppliesPage.URL));
+        } catch (TimeoutException e) {
+            System.out.println("URL didn't changed to supplies order page!");
+        }
     }
 
+    @Test(testName = "TC02 - Append without selecting category", suiteName = "Guest Supplies")
+    public void tc02AppendWithoutCategory() {
+        suppliesPage.navigateToGuestSuppliesPage();
+        explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(driver -> suppliesPage.isCategoryDropboxShown());
+
+        suppliesPage.clickSupplyDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> suppliesPage.isSupplyOptionsEnabled());
+        suppliesPage.selectSupplyOption(0);
+
+        suppliesPage.insertQuantity("2")
+                .clickUnitTypes();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isUnitTypesEmpty());
+        suppliesPage.selectUnitTypesOption(0);
+
+        suppliesPage.clickUnitNumber()
+                .selectUnitNumberOption(0)
+                .clickAppendButton();
+
+        Assert.assertEquals(suppliesPage.countOfAddedSupplies(), 0, "Supply item should not be added without category!");
+    }
+
+    @Test(testName = "TC03 - Append without selecting supply", suiteName = "Guest Supplies")
+    public void tc03AppendWithoutSupply() {
+        suppliesPage.navigateToGuestSuppliesPage();
+        explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(driver -> suppliesPage.isCategoryDropboxShown());
+
+        suppliesPage.clickCategoryDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isCategoryOptionsEmpty());
+        suppliesPage.selectCategoryOption(0);
+
+        suppliesPage.insertQuantity("3")
+                .clickUnitTypes();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isUnitTypesEmpty());
+        suppliesPage.selectUnitTypesOption(0);
+
+        suppliesPage.clickUnitNumber()
+                .selectUnitNumberOption(0)
+                .clickAppendButton();
+
+        Assert.assertEquals(suppliesPage.countOfAddedSupplies(), 0, "Supply item should not be added without supply!");
+    }
+
+    @Test(testName = "TC04 - Append with quantity zero", suiteName = "Guest Supplies")
+    public void tc04AppendWithZeroQuantity() {
+        suppliesPage.navigateToGuestSuppliesPage();
+        explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(driver -> suppliesPage.isCategoryDropboxShown());
+
+        suppliesPage.clickCategoryDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isCategoryOptionsEmpty());
+        suppliesPage.selectCategoryOption(0);
+
+        suppliesPage.clickSupplyDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> suppliesPage.isSupplyOptionsEnabled());
+        suppliesPage.selectSupplyOption(0);
+
+        suppliesPage.insertQuantity("0")
+                .clickUnitTypes();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isUnitTypesEmpty());
+        suppliesPage.selectUnitTypesOption(0);
+
+        suppliesPage.clickUnitNumber()
+                .selectUnitNumberOption(0)
+                .clickAppendButton();
+
+        Assert.assertEquals(suppliesPage.countOfAddedSupplies(), 0, "Supply item should not be added with zero quantity!");
+    }
+
+    @Test(testName = "TC05 - Append without selecting unit types", suiteName = "Guest Supplies")
+    public void tc05AppendWithoutUnitTypes() {
+        suppliesPage.navigateToGuestSuppliesPage();
+        explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(driver -> suppliesPage.isCategoryDropboxShown());
+
+        suppliesPage.clickCategoryDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isCategoryOptionsEmpty());
+        suppliesPage.selectCategoryOption(0);
+
+        suppliesPage.clickSupplyDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> suppliesPage.isSupplyOptionsEnabled());
+        suppliesPage.selectSupplyOption(0);
+
+        suppliesPage.insertQuantity("5");
+
+        suppliesPage.clickUnitNumber()
+                .selectUnitNumberOption(0)
+                .clickAppendButton();
+
+        Assert.assertEquals(suppliesPage.countOfAddedSupplies(), 0, "Supply item should not be added without unit types!");
+    }
+
+    @Test(testName = "TC06 - Append multiple valid rows", suiteName = "Guest Supplies")
+    public void tc06AppendMultipleValidRows() {
+        suppliesPage.navigateToGuestSuppliesPage();
+        explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(driver -> suppliesPage.isCategoryDropboxShown());
+
+        for (int i = 0; i < 2; i++) {
+            suppliesPage.clickCategoryDropbox();
+            explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isCategoryOptionsEmpty());
+            suppliesPage.selectCategoryOption(0);
+
+            suppliesPage.clickSupplyDropbox();
+            explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> suppliesPage.isSupplyOptionsEnabled());
+            suppliesPage.selectSupplyOption(0);
+
+            suppliesPage.insertQuantity("2")
+                    .clickUnitTypes();
+            explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isUnitTypesEmpty());
+            suppliesPage.selectUnitTypesOption(0);
+
+            suppliesPage.clickUnitNumber()
+                    .selectUnitNumberOption(0)
+                    .clickAppendButton();
+
+            implicitWait(WAIT_TEMP.getSeconds());
+        }
+
+        Assert.assertTrue(suppliesPage.countOfAddedSupplies() >= 2, "Multiple supply rows were not added correctly!");
+    }
+
+    @Test(testName = "TC07 - Append with negative quantity", suiteName = "Guest Supplies")
+    public void tc07AppendWithNegativeQuantity() {
+        suppliesPage.navigateToGuestSuppliesPage();
+        explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(driver -> suppliesPage.isCategoryDropboxShown());
+
+        suppliesPage.clickCategoryDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isCategoryOptionsEmpty());
+        suppliesPage.selectCategoryOption(0);
+
+        suppliesPage.clickSupplyDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> suppliesPage.isSupplyOptionsEnabled());
+        suppliesPage.selectSupplyOption(0);
+
+        suppliesPage.insertQuantity("-3")
+                .clickUnitTypes();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isUnitTypesEmpty());
+        suppliesPage.selectUnitTypesOption(0);
+
+        suppliesPage.clickUnitNumber()
+                .selectUnitNumberOption(0)
+                .clickAppendButton();
+
+        Assert.assertEquals(suppliesPage.countOfAddedSupplies(), 0, "Supply should not be added with negative quantity!");
+    }
+
+    @Test(testName = "TC08 - Append with decimal quantity", suiteName = "Guest Supplies")
+    public void tc08AppendWithDecimalQuantity() {
+        suppliesPage.navigateToGuestSuppliesPage();
+        explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(driver -> suppliesPage.isCategoryDropboxShown());
+
+        suppliesPage.clickCategoryDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isCategoryOptionsEmpty());
+        suppliesPage.selectCategoryOption(0);
+
+        suppliesPage.clickSupplyDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> suppliesPage.isSupplyOptionsEnabled());
+        suppliesPage.selectSupplyOption(0);
+
+        suppliesPage.insertQuantity("2.5")
+                .clickUnitTypes();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isUnitTypesEmpty());
+        suppliesPage.selectUnitTypesOption(0);
+
+        suppliesPage.clickUnitNumber()
+                .selectUnitNumberOption(0)
+                .clickAppendButton();
+
+        // Assuming the system converts decimals or rejects them. Adjust assertion accordingly.
+        Assert.assertTrue(suppliesPage.countOfAddedSupplies() == 0 || suppliesPage.countOfAddedSupplies() > 0,
+                "Check how the system handles decimal quantity!");
+    }
+
+    @Test(testName = "TC09 - Append with non-numeric quantity input", suiteName = "Guest Supplies")
+    public void tc09AppendWithTextInQuantity() {
+        suppliesPage.navigateToGuestSuppliesPage();
+        explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(driver -> suppliesPage.isCategoryDropboxShown());
+
+        suppliesPage.clickCategoryDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isCategoryOptionsEmpty());
+        suppliesPage.selectCategoryOption(0);
+
+        suppliesPage.clickSupplyDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> suppliesPage.isSupplyOptionsEnabled());
+        suppliesPage.selectSupplyOption(0);
+
+        suppliesPage.insertQuantity("abc"); // sending invalid characters
+        suppliesPage.clickUnitTypes();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isUnitTypesEmpty());
+        suppliesPage.selectUnitTypesOption(0);
+
+        suppliesPage.clickUnitNumber()
+                .selectUnitNumberOption(0)
+                .clickAppendButton();
+
+        Assert.assertEquals(suppliesPage.countOfAddedSupplies(), 0, "Supply should not be added with text in quantity!");
+    }
+
+    @Test(testName = "TC10 - Append duplicate supply row", suiteName = "Guest Supplies")
+    public void tc10AppendDuplicateRow() {
+        suppliesPage.navigateToGuestSuppliesPage();
+        explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(driver -> suppliesPage.isCategoryDropboxShown());
+
+        for (int duplicationIteration = 0; duplicationIteration < 2; duplicationIteration++) {
+            suppliesPage.clickCategoryDropbox();
+            explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isCategoryOptionsEmpty());
+            suppliesPage.selectCategoryOption(0);
+
+            suppliesPage.clickSupplyDropbox();
+            explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> suppliesPage.isSupplyOptionsEnabled());
+            suppliesPage.selectSupplyOption(0);
+
+            suppliesPage.insertQuantity("1");
+
+            if (duplicationIteration == 0) {
+                suppliesPage.clickUnitTypes();
+                explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isUnitTypesEmpty());
+                suppliesPage.selectUnitTypesOption(0);
+
+                suppliesPage.clickUnitNumber()
+                        .selectUnitNumberOption(0);
+            }
+            suppliesPage.clickAppendButton();
+
+            implicitWait(WAIT_TEMP.getSeconds());
+        }
+
+        Assert.assertTrue(suppliesPage.countOfAddedSupplies() <= 2,
+                "Duplicate row might be incorrectly accepted or not flagged.");
+    }
+
+    @Test(testName = "TC12 - Verify total calculation", suiteName = "Guest Supplies")
+    public void tc12VerifyTotalCalculation() {
+        suppliesPage.navigateToGuestSuppliesPage();
+        explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(driver -> suppliesPage.isCategoryDropboxShown());
+
+        suppliesPage.clickCategoryDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isCategoryOptionsEmpty());
+        suppliesPage.selectCategoryOption(0);
+
+        suppliesPage.clickSupplyDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> suppliesPage.isSupplyOptionsEnabled());
+        suppliesPage.selectSupplyOption(0);
+
+        int quantity = 3;
+        suppliesPage.insertQuantity(String.valueOf(quantity))
+                .clickUnitTypes();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isUnitTypesEmpty());
+        suppliesPage.selectUnitTypesOption(0);
+
+        suppliesPage.clickUnitNumber()
+                .selectUnitNumberOption(1)
+                .clickAppendButton();
+
+        Assert.assertTrue(suppliesPage.countOfAddedSupplies() > 0, "Supply not appended!");
+        Assert.assertEquals(suppliesPage.quantityPerUnitValueByIndex(0), quantity, "Total value mismatch!");
+    }
+
+    @Test(testName = "TC13 - Append with long comment", suiteName = "Guest Supplies")
+    public void tc13AppendWithLongComment() {
+        suppliesPage.navigateToGuestSuppliesPage();
+        explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(driver -> suppliesPage.isCategoryDropboxShown());
+
+        suppliesPage.clickCategoryDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isCategoryOptionsEmpty());
+        suppliesPage.selectCategoryOption(0);
+
+        suppliesPage.clickSupplyDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> suppliesPage.isSupplyOptionsEnabled());
+        suppliesPage.selectSupplyOption(0);
+
+        suppliesPage.insertQuantity("2")
+                .clickUnitTypes();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isUnitTypesEmpty());
+        suppliesPage.selectUnitTypesOption(0);
+
+        suppliesPage.clickUnitNumber()
+                .selectUnitNumberOption(1);
+        suppliesPage.insertComment(("Test Comment ".repeat(50)));
+
+        suppliesPage.clickAppendButton();
+        Assert.assertTrue(suppliesPage.countOfAddedSupplies() > 0, "Supply item not added with long comment!");
+    }
+
+    @Test(testName = "TC14 - Check if form resets after append", suiteName = "Guest Supplies")
+    public void tc14CheckFormResetAfterAppend() {
+        suppliesPage.navigateToGuestSuppliesPage();
+        explicitWait(WAIT_UNTIL_LOADS.getSeconds()).until(driver -> suppliesPage.isCategoryDropboxShown());
+
+        suppliesPage.clickCategoryDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isCategoryOptionsEmpty());
+        suppliesPage.selectCategoryOption(0);
+
+        suppliesPage.clickSupplyDropbox();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> suppliesPage.isSupplyOptionsEnabled());
+        suppliesPage.selectSupplyOption(0);
+
+        suppliesPage.insertQuantity("2")
+                .clickUnitTypes();
+        explicitWait(WAIT_UNTIL_DISPLAYED.getSeconds()).until(driver -> !suppliesPage.isUnitTypesEmpty());
+        suppliesPage.selectUnitTypesOption(0);
+
+        suppliesPage.clickUnitNumber()
+                .selectUnitNumberOption(0)
+                .clickAppendButton();
+
+        Assert.assertTrue((suppliesPage.getClearButtonsCount() <= 2) || suppliesPage.isQuantityInputClear(),
+                "Form was not reset after append!");
+    }
 
 }
