53ff97ff62b294bfeff9908aeef731487cedd3b4
mai_ma <maymahdy1248@gamil.com>
2025-04-09 11:18:39 +0200

    new changes

--- src/test/java/nazeel/pages/SuppliesUnitUsagesPage.java
+++ src/test/java/nazeel/pages/SuppliesUnitUsagesPage.java
@@ -15,7 +15,7 @@ public class SuppliesUnitUsagesPage {
     private final By Add_New_Unit_Usage = By.xpath("//div[2]/div/button");
     private final By dropdownLocator = By.xpath("//div/kendo-searchbar/input");
     private final By listItemText = By.xpath("//div/kendo-searchbar/input");
-    private final By listIteeeeeeeeeemText = By.xpath("//div/kendooooooooooooooooooo-searchbar/input");
+    private final By maiele = By.xpath("//diviii");
 
   // Actions
     public void Click_Add_New_Unit_Usage_buttons() {
