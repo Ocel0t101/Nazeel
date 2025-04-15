3278c2943574ed4824b7fff27a856a1dcaa34052
mai ahmed <maymahdy1248@gmail.com>
2025-04-15 15:44:22 +0200

    new changes

--- src/test/java/nazeel/pages/DashboardPage.java
+++ src/test/java/nazeel/pages/DashboardPage.java
@@ -1,14 +1,25 @@
 package nazeel.pages;
 
+import nazeel.utils.DelayedWebDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static nazeel.TestBase.getRootDriver;
 
-public class DashboardPage {
+public class DashboardPage extends SoftAssert {
 
     //Locators
-    private final By userVerificationLaterButton = By.xpath("//*[contains(text(),'Later')]");
     private static final By SetupMenu = By.xpath("//a[contains(text(),'Setup')]");
     private static final By SuppliesUnitUsages = By.xpath("//div[15]/a[3]");
     private static final By SUsages = By.xpath("//a[12]");
@@ -17,19 +28,44 @@ public class DashboardPage {
         getRootDriver().findElement(userVerificationLaterButton).click(); }
     */
 
-    public static void OpenSuppliesUnitUsagesPage() throws InterruptedException {
+    public  void OpenSuppliesUnitUsagesPage() throws InterruptedException {
         getRootDriver().findElement(SetupMenu).click();
         Thread.sleep(3000);
 
-//        WebElement element = getRootDriver().findElement( By.xpath("//a[12]"));
-//        ((JavascriptExecutor) getRootDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
-//        element.click();
+        WebElement element = getRootDriver().findElement( By.xpath("//a[12]"));
+
+        DelayedWebDriver.DelayedWebElement delayedWebElement = new DelayedWebDriver.DelayedWebElement(element , 1000);
+
+         element = delayedWebElement.getWrappedElement();
+
+        JavascriptExecutor jsExecutor;
+
+            jsExecutor = (JavascriptExecutor) getRootDriver();
+
+        jsExecutor.executeScript("arguments[0].click();", element);
+
+        jsExecutor.executeScript("arguments[0].click();", element);
+
+        ((JavascriptExecutor) getRootDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
 
         getRootDriver().findElement(SUsages).click();
 
         Thread.sleep(1000);
 
         getRootDriver().findElement(SuppliesUnitUsages).click();
+    }
 
+    public void AssertToastMessagesContains(String actualToastText) {
+        //WebElement expectedToast= getRootDriver().findElement(By.cssSelector("div.toast-message"));
+
+        WebDriverWait wait = new WebDriverWait(getRootDriver(), Duration.ofSeconds(10));
+        WebElement expectedToast = wait.until(ExpectedConditions.refreshed(
+                ExpectedConditions.visibilityOfElementLocated(By.className("toast-message"))
+        ));
+        String expectedToastText = expectedToast
+                .getText()
+                        .trim();
+        // Assert the toast text
+        Assert.assertEquals(actualToastText, expectedToastText, "Toast message did not match!");
     }
 }
--- src/test/java/nazeel/test_suites/Setup_Menus/SuppliesUnitUsagesClass.java
+++ src/test/java/nazeel/test_suites/Setup_Menus/SuppliesUnitUsagesClass.java
@@ -1,6 +1,6 @@
-package nazeel.test_suites;
+package nazeel.test_suites.Setup_Menus;
 
-import nazeel.TestBase;
+import nazeel.base.TestBase;
 import nazeel.pages.DashboardPage;
 import nazeel.pages.SuppliesUnitUsagesPage;
 import nazeel.pages.LoginPage;
@@ -8,54 +8,51 @@ import org.testng.annotations.Test;
 
 public class SuppliesUnitUsagesClass extends TestBase {
 
-    @Test(priority = 1)
-    public void testLogin() throws InterruptedException {
+
+   @Test (priority = 1 , testName = "TC01 - Login", suiteName = "Guest Supplies Usage")
+    public void TC01_testLogin() throws Exception {
         LoginPage loginPage = new LoginPage();
         loginPage.insertUsername("maiow");
         loginPage.insertPassword("123456Mm&&");
         loginPage.insertAccessCode("01373");
         loginPage.clickLoginButton();
         Thread.sleep(4000);
-
         //** Select Property **//****
-
         loginPage.Select_One_Property_("P01558");
+        Thread.sleep(3000);
+        //loginPage.userVerificationLaterButton_click();
+    }
 
-           // to be in Test= 2
+    @Test (priority = 2, testName = "TC02 - AddNew_SuppliesUnitUsages", suiteName = "Guest Supplies Usage")
+    public void TC02_AddNew_SuppliesUnitUsages() throws Exception {
 
-        Thread.sleep(4000);
-
-        //**  Open clickOnSuppliesUnitUsagesPage**//****
+     //   Thread.sleep(4000);
 
         DashboardPage DashboardPage = new DashboardPage();
 
         DashboardPage.OpenSuppliesUnitUsagesPage();
-
         Thread.sleep(4000);
-
         //** open Add new record here !!
-
         SuppliesUnitUsagesPage SuppliesUnitUsagesPage = new SuppliesUnitUsagesPage();
-
         SuppliesUnitUsagesPage.Click_Add_New_Unit_Usage_buttons();
-
+        Thread.sleep(7000);
+        SuppliesUnitUsagesPage.Add_New_Guest_Supplies_bySelectUnitType("Room with Hall");
         Thread.sleep(4000);
-        try {
+        SuppliesUnitUsagesPage.Add_New_Guest_Supplies_bySelectCategory("meals");
+        Thread.sleep(3000);
+        SuppliesUnitUsagesPage.Add_New_Guest_Supplies_bySelectSupply("measlfirst");
+        Thread.sleep(1000);
+        SuppliesUnitUsagesPage.Enter_Daily_Qty_AND_Monthly_Qty("1","2");
+        Thread.sleep(1000);
+        SuppliesUnitUsagesPage.Append();
+        Thread.sleep(1000);
+       SuppliesUnitUsagesPage.Save();
+        Thread.sleep(1000);
+        //Saved Successfully
+        DashboardPage.AssertToastMessagesContains("tttes");
 
-            SuppliesUnitUsagesPage.selectListItem("Single Room");
-
-        } catch (Exception e) {
-
-            throw new RuntimeException(e);
-        }
-
-      /*  @Test(priority = 2)
-        public void Add_UnitUsages() throws InterruptedException {
-
-            }
-        }
-
-       */
     }
 }
+
+
 
